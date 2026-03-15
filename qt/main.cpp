55f6d4faf1dc9ba7d02eb22bedcf6743939103e6
Vishavjeet Bawa <nap07lean@gmail.com>
2026-03-15 16:01:45 +0530

    UI added

---
+++ qt/main.cpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QWidget>
+
+class QLineEdit;
+class QLabel;
+class QPushButton;
+class QComboBox;
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Window(QWidget *parent = nullptr);
+
+private slots:
+    void runEngine();
+
+private:
+
+    QLineEdit* s0Box;
+    QLineEdit* kBox;
+    QLineEdit* tBox;
+    QLineEdit* rBox;
+    QLineEdit* sigmaBox;
+    QLineEdit* nBox;
+    QLineEdit* mBox;
+
+    QComboBox* engineBox;
+
+    QLabel* priceLabel;
+    QLabel* stderrLabel;
+    QLabel* timeLabel;
+
+    QPushButton* runButton;
+};
